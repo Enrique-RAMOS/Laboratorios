cdca474715930761b785928af4a45d177d7d7e54
Enrique-RAMOS <84140676+Enrique-RAMOS@users.noreply.github.com>
2022-02-26 23:20:18 -0600

    New

---
+++ Lab1_Notas/Lab1_Notas/Bussines/Operaciones.cs
@@ -0,0 +1,25 @@
+﻿using Lab1_Notas.Models;
+namespace Lab1_Notas.Bussines
+{
+    public class Operaciones
+    {
+        public double index(Acciones acciones)
+        {
+
+
+
+            Double promedio1 = acciones.lab1 * 0.4 + acciones.par1 * 0.6;
+            Double promedio2 = acciones.lap2 * 0.4 + acciones.par2 * 0.6;
+            Double promedio3 = acciones.lap3 * 0.4 + acciones.par3 * 0.6;
+
+            Double calculo = promedio1 * 0.333333 + promedio2 * 0.333333 + promedio3 * 0.333333;
+
+            string nombres = acciones.nombre;
+
+            return calculo;
+           
+        }
+      
+
+    }
+}
---
+++ Lab1_Notas/Lab1_Notas/Models/Acciones.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab1_Notas.Models
+{
+    public class Acciones
+    {
+        public string nombre { get; set; }
+        public Double lab1 { get; set; }
+        public Double lap2 { get; set; }
+        public Double lap3 { get; set; }
+        public Double par1 { get; set; }
+        public Double par2 { get; set; }
+        public Double par3 { get; set; }
+    }
+}
